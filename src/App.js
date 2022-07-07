993dd6ab5560148aa2e91a94478043659cf6a823
Tiago <ztiagok@gmail.com>
2022-07-07 15:17:19 -0500

    c0 - Início do Requisito 1

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
+        <p>Edit src/App.js and ioi save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          .
         >
           Learn React
         </a>
