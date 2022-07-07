5d37add233e3c48105b5710e8dbd2e2ce3ecd767
Tiago <ztiagok@gmail.com>
2022-07-07 18:10:54 -0500

    c3 - Finalização do Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <App />,
   document.getElementById('root'),
 );
 
