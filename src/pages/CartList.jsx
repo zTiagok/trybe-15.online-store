f3deb57882682592d4f35c8419d7109a005988d0
Tiago <ztiagok@gmail.com>
2022-07-13 16:10:55 -0500

    Correção dos Requisitos 3, 8 e 9

--- src/pages/CartList.jsx
+++ src/pages/CartList.jsx
@@ -4,7 +4,7 @@ import CartProduct from './CartProduct';
 
 export default class CartList extends Component {
   state = {
-    arrayIDs: [],
+    arrayIDs: null,
   }
 
   componentDidMount() {
@@ -15,55 +15,30 @@ export default class CartList extends Component {
     const itemsIDs = localStorage.getItem('itemId');
     const filteredIDs = JSON.parse(itemsIDs);
     this.setState({ arrayIDs: filteredIDs });
-
-    // console.log('linha 19', itemsIDs);
-    // itemsIDs.reduce((acc, element) => {
-    //   if (element === acc) {
-    //     this.setState((prevState) => ({
-    //       quantity: [...prevState.quantity, +1],
-    //     }));
-    //     // return quantityItens;
-    //   }
-    //   return acc;
-    // });
-    // const arrayToString = productArray.toString();
-    // let conc = 0;
-    // itemsIDs.map((id) => {
-    //   const data = this.getAPI(id);
-
-    //   if (id === arrayToString) {
-    //     conc += 1;
-    //     this.setState({ quantity: conc });
-    //   }
-    //   this.setState({ name: data.title });
-    // });
-    // const arrayFiltred = itemsIDs.filter((element) => element !== arrayToString);
-    // ;
   }
 
   render() {
     const { arrayIDs } = this.state;
-    const arrayLengthMessage = (
-      <div data-testid="shopping-cart-product-quantity">
-        {`Quantidade ${arrayIDs.length}`}
-      </div>
-    );
+
+    if (arrayIDs) {
+      this.arrayLengthMessage = (
+        <div data-testid="shopping-cart-product-quantity">
+          {`Quantidade ${arrayIDs.length}`}
+        </div>
+      );
+    }
 
     return (
       <>
-        {arrayIDs.map((id, index) => (<CartProduct
+        {arrayIDs
+        && arrayIDs.map((id, index) => (<CartProduct
           productID={ id }
           key={ index }
         />))}
 
-        {/* {array.map((id) => (<CartProduct
-          productName={getProduct(id)
-            .then((data) => console.log(data.title))}
-          key={id}
-        />))} */}
-
-        {arrayIDs.length
-          ? arrayLengthMessage
+        {arrayIDs
+        && arrayIDs.length
+          ? this.arrayLengthMessage
           : <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>}
       </>
     );
